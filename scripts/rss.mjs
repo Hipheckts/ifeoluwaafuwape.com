47fda523ea8f07c50f72bcb5a4d1d6da7e31ff5f
Afuwape Ifeoluwa <hipheckt@gmail.com>
2022-05-13 03:57:35 +0100

    content layer

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -4,15 +4,15 @@ import { allBlogs } from '.contentlayer/data';
 
 async function generate() {
   const feed = new RSS({
-    title: 'Segun Adebayo',
-    site_url: 'https://adebayosegun.com',
-    feed_url: 'https://adebayosegun.com/feed.xml',
+    title: 'Ifeoluwa Afuwape',
+    site_url: 'https://ifeoluwaafuwape.com',
+    feed_url: 'https://ifeoluwaafuwape.com/feed.xml',
   });
 
   allBlogs.map((blog) => {
     feed.item({
       title: blog.title,
-      url: `https://adebayosegun.com/blog/${blog.slug}`,
+      url: `https://ifeoluwaafuwape.com/blog/${blog.slug}`,
       date: blog.publishedAt,
       description: blog.description,
     });
