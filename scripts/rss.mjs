422120158a04f2d3e75c2af56043d381ba6fe753
Afuwape Ifeoluwa <hipheckt@gmail.com>
2022-05-18 05:37:57 +0100

    build

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -1,6 +1,6 @@
 import { writeFileSync } from 'fs';
 import RSS from 'rss';
-import { allBlogs } from '.contentlayer/data';
+// import { allBlogs } from '.contentlayer/data';
 
 async function generate() {
   const feed = new RSS({
@@ -9,14 +9,14 @@ async function generate() {
     feed_url: 'https://ifeoluwaafuwape.com/feed.xml',
   });
 
-  allBlogs.map((blog) => {
-    feed.item({
-      title: blog.title,
-      url: `https://ifeoluwaafuwape.com/blog/${blog.slug}`,
-      date: blog.publishedAt,
-      description: blog.description,
-    });
-  });
+  // allBlogs.map((blog) => {
+  //   feed.item({
+  //     title: blog.title,
+  //     url: `https://ifeoluwaafuwape.com/blog/${blog.slug}`,
+  //     date: blog.publishedAt,
+  //     description: blog.description,
+  //   });
+  // });
 
   writeFileSync('./public/feed.xml', feed.xml({ indent: true }));
 }
